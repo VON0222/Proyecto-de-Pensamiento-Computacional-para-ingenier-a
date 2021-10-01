384d652cf984383cbaf8224baa00579f63b23dcd
VON0222 <88916058+VON0222@users.noreply.github.com>
2021-09-30 22:14:50 -0500

    Add files via upload

--- Genshin pity.py
+++ Genshin pity.py
@@ -1,3 +1,6 @@
+from random import uniform
+from random import randint
+
 #Esta función da a elegir al usuario el banner del cual obtener información.
 
 def info_banner():
@@ -67,6 +70,67 @@ def pity():
     elif deseostot >= 90:
         print("Tienes suficientes deseos para activar el sistema de pity")
 
+#Esta función hace una simulación del sistema de gacha del juego de un deseo a la vez, puede ayudar a los jugadores
+#adictos a esta mecanica sin que necesiten gastar su moneda del juego.
+
+def gacha():
+    banner = input("En que banner quieres desear?\nBanner permanente (opcion 1)\nBanner de armas (opcion 2)\n"   
+                    "Banner de promocion (opcion 3)\n")
+    if banner == "1":
+        chara5 = ["Jean","Diluc","Mona","Keching","Qiqi" ]
+        chara5num = randint(0,5)
+        weap5 = ["Arco de amos","Alas celestiales","Oración Perdida a los Vientos Sagrados","Pergamino Celestial"]
+        weap5num = randint(0,4)
+        chara4 = ["Diona","Razor","Beidou","Bennet"]
+        chara4num = randint(0,4)
+        weap4 = ["Herrumbre","Arco del sacrificio","Ultimo acorde","Ojo de la perspicacia"]
+        weap4num = randint(0,4)
+        prob = uniform(0,100)
+        if prob <= 0.3:
+            print("Obtuviste", chara5[chara5num])
+        elif (prob <= 0.6) and (prob > 0.3):
+            print("Obtuviste", weap5[weap5num])
+        elif (prob <= 3.15) and (prob > 0.6):
+            print("Obtuviste", chara4[chara4num])
+        elif (prob <= 5.7) and (prob > 3.15):
+            print("Obtuviste", weap4[weap4num])
+        else:
+            print("Obtuviste un arma de 3 estrellas :(")
+
+    elif banner == "2":
+        weap5 = ["Luna inalterable","Cortador de Jade primordial","Halcón de jade","Lápida del lobo"]
+        weap5num = randint(0, 4)
+        chara4 = ["Diona", "Razor", "Beidou", "Bennet"]
+        chara4num = randint(0, 4)
+        weap4 = ["Códice de Favonius","Segadora de la lluvia","Perdición del dragon","Gran espada de sacrificio"]
+        weap4num = randint(0, 4)
+        prob = uniform(0, 100)
+        if prob <= 0.7:
+            print("Obtuviste", weap5[weap5num])
+        elif (prob <= 3.7) and (prob > 0.7):
+            print("Obtuviste", weap4[weap4num])
+        elif (prob <= 6.7) and (prob > 3.7):
+            print("Obtuviste", chara4[chara4num])
+        else:
+            print("Obtuviste un arma de 3 estrellas :(")
+
+    elif banner == "3":
+        chara5 = ["Sangonomiya","Jean","Diluc","Mona","Keching","Qiqi"]
+        chara5num = randint(0, 6)
+        chara4 = ["Diona", "Xingchiu", "Beidou", "Rosaria"]
+        chara4num = randint(0, 4)
+        weap4 = ["Flauta","Rugido del leon","Espada del tiempo","Memorias de sacrificios"]
+        weap4num = randint(0, 4)
+        prob = uniform(0, 100)
+        if prob <= 0.6:
+            print("Obtuviste", chara5[chara5num])
+        elif (prob <= 3.15) and (prob > 0.6):
+            print("Obtuviste", weap4[weap4num])
+        elif (prob <= 5.7) and (prob > 3.15):
+            print("Obtuviste", chara4[chara4num])
+        else:
+            print("Obtuviste un arma de 3 estrellas :(")
+
 #Frase mencionada al inicio del programa para proveer contexto al usuario.
 
 print("En genshin impact hay un sistema de gacha el cual tiene un sistema de pity,\nel cual te garantiza un personaje "
@@ -78,10 +142,12 @@ print("En genshin impact hay un sistema de gacha el cual tiene un sistema de pit
 #y el programa se detendra.
 
 reinicio = "si"
+reiniciogacha = "si"
 
 while reinicio == "si":
     funcion = input("Que quieres hacer?\nChecar tus probabilidades (opcion 1)\nCalcular tus deseos faltantes para un "
-                    "personaje de 5 estrellas (opcion 2)\nCalcular cuantos destinos puedes obtener (opcion 3)\n")
+                    "personaje de 5 estrellas (opcion 2)\nCalcular cuantos destinos puedes obtener (opcion 3)\n"
+                    "Simulación de gacha <NEW> (opcion 4)\n")
     if funcion == "1":
         info_banner()
         reinicio = input("Volver al menu?\n")
@@ -90,6 +156,11 @@ while reinicio == "si":
         reinicio = input("Volver al menu?\n")
     elif funcion == "3":
         destinos()
+        reinicio = input("Volver al menu?\n")
+    elif funcion == "4":
+        while reiniciogacha == "si":
+            gacha()
+            reiniciogacha = input("Deseas probar tu suerte de nuevo?\n")
         reinicio = input("Volver al menu?\n")
     else:
         print("No hay esa opcion")
